6f883178c8c2b8b89262f93e2811dbfcf9125adf
KotelczukB <WhiteWolf12>
2020-07-04 18:28:57 +0200

    remove separate chat

--- realtime_cluster/game_server/test/services/backends.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../src/app';
-
-describe('\'backends\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('backends');
-    expect(service).toBeTruthy();
-  });
-});
