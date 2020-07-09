7fc09192b4665aa343b4d7a43387749e2ef25749
KotelczukB <WhiteWolf12>
2020-07-09 13:02:41 +0200

    remove Backend

--- game/game-backend/test/services/players.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../src/app';
-
-describe('\'players\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('players');
-    expect(service).toBeTruthy();
-  });
-});
