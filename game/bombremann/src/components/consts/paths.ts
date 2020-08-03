72bf2734d533381cdcae0cb4b53ff2f9049fb127
KotelczukB <WhiteWolf12>
2020-08-03 11:18:46 +0200

    naming and root compose

--- game/bombremann/src/components/consts/paths.ts
+++ game/bombremann/src/components/consts/paths.ts
@@ -1,2 +1,2 @@
-export const BACKEND="http://host.docker.internal:8080";
+export const BACKEND="http://192.168.10.4:8080";
 export const ROUTER=`${window.location.protocol}//${window.location.hostname}:3080/users`;
